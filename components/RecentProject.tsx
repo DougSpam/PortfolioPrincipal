b6f2bc36e6c4c3089da5aa197fcbaae9ade89595
DougSpam <ddougllasbezerra@gmail.com>
2024-07-09 15:48:19 -0300

    projects

--- components/RecentProject.tsx
+++ components/RecentProject.tsx
@@ -1,4 +1,6 @@
+import { projects } from '@/data'
 import React from 'react'
+import { PinContainer } from './ui/3d-pin'
 
 const RecentProject = () => {
   return (
@@ -8,7 +10,22 @@ const RecentProject = () => {
           <span className='text-purple'>Projetos recentes</span>
         </h1>
         <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
-
+          {projects.map(({id, title , des, img, iconLists, link}) => (
+            <div key={id} className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]'>
+              <PinContainer title={title} href={link}>
+                <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh]'>
+                  <div>
+                    <img src='/bg.png' alt='.'/>
+                  </div>
+                  <img 
+                    src={img} 
+                    alt={title} 
+                    className='z-10 absolute bottom-0'
+                  />
+                </div>
+              </PinContainer>
+           </div>
+          ))}
         </div>
     </div>
   )
