65d4df5e276cd6329160d5df8323f0d1ba3e61e3
DougSpam <ddougllasbezerra@gmail.com>
2024-07-09 00:37:04 -0300

    a

---
+++ components/RecentProject.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const RecentProject = () => {
+  return (
+    <div className='py-20'>
+        <h1 className='heading'>
+        Uma pequena seleção de {' '}
+          <span className='text-purple'>Projetos recentes</span>
+        </h1>
+        <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
+
+        </div>
+    </div>
+  )
+}
+
+export default RecentProject
