c7be9d954b85cb5b3e348566bf4e25097c864806
DougSpam <ddougllasbezerra@gmail.com>
2024-07-10 01:08:48 -0300

    Atualizacao

--- components/RecentProject.tsx
+++ components/RecentProject.tsx
@@ -13,8 +13,8 @@ const RecentProject = () => {
           {projects.map(({id, title , des, img, iconLists, link}) => (
             <div key={id} className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]'>
               <PinContainer title={title} href={link}>
-                <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh]'>
-                  <div>
+                <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10'>
+                  <div className=' relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
                     <img src='/bg.png' alt='.'/>
                   </div>
                   <img 
@@ -22,6 +22,19 @@ const RecentProject = () => {
                     alt={title} 
                     className='z-10 absolute bottom-0'
                   />
+                </div>
+                <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1 '>
+                  {title}
+                </h1>
+
+                <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>
+                  {des}
+                </p>
+
+                <div className='flex items-center justify-between mt-7 mb-3'>
+                  <div className='flex items-center'>
+
+                  </div>
                 </div>
               </PinContainer>
            </div>
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -111,7 +111,7 @@ export const BentoGridItem = ({
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-1 top-3 lg:-right-2"> 
               <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
                 {['React.js' , 'Next.js', 'Node.js', 'JavaScript'].map((item) => (
-                  <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                  <span key={item} className="py-2 lg:py-1.5 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
                     {item}
                   </span>
                 ))}
@@ -119,7 +119,7 @@ export const BentoGridItem = ({
 
               <div className="flex flex-col gap-3 lg:gap-8">
                 {['Html' , 'Css', 'Php', 'Sql'].map((item) => (
-                  <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                  <span key={item} className="py-2 lg:py-1.5 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
                     {item}
                   </span>
                 ))}
