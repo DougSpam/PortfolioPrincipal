9d58a677b3ec544d422bfb17c566ef1798f47d33
DougSpam <ddougllasbezerra@gmail.com>
2024-07-10 23:43:43 -0300

    a

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -119,13 +119,13 @@ export const BentoGridItem = ({
 
               <div className="flex flex-col gap-3 lg:gap-8">
                 {['Html' , 'Css', 'Php', 'Sql'].map((item) => (
-                  <span key={item} className="py-2 lg:py-1.5 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
-                    {item}
-                  </span>
-                ))}
+                    <span key={item} className="py-2 lg:py-1.5 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                      {item}
+                    </span>
+                  ))}
+                </div>
               </div>
-            </div>
-          )}
+            )}
 
           { id === 6 && (
             <div className="mt-5 relative"> 
